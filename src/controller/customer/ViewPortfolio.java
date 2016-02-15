7136d27cbe6a2c22c4e4349ba5a87dc15170c486
Paranjay Sharma <paranjas@andrew.cmu.edu>
2016-02-14 21:38:24 -0500

    Create ViewPortfolio.java

---
+++ src/controller/customer/ViewPortfolio.java
@@ -0,0 +1,31 @@
+package controller.customer;
+import java.util.List;
+import java.util.ArrayList;
+
+public class ViewPortfolio {
+	private String name;
+	private String shares;
+	private double price;
+	List<ViewPortfolio> list = new ArrayList<ViewPortfolio>();
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getShares() {
+		return shares;
+	}
+	public void setShares(String shares) {
+		this.shares = shares;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String toString() {
+		return "name: " + name + " , shares: " + shares + " , price: " + price; 
+	}
+}
