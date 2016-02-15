2e2513a7a2a83f16e895731aaf02e28396d80bd0
Paranjay Sharma <paranjas@andrew.cmu.edu>
2016-02-15 12:51:30 -0500

    Updated Request Check Web Service

--- src/controller/customer/CustomerRequestCheckAction.java
+++ src/controller/customer/CustomerRequestCheckAction.java
@@ -23,16 +23,19 @@ import databean.CustomerBean;
 public class CustomerRequestCheckAction extends Action {
 
 	private static final String FORMAT_STRING = "###,###,###,###,###,##0.00";
-	//private static final String ACTION_NAME = "customer-request-check.do";
-	//private static final String REQUES_CHECK_JSP = "customer-request-check.jsp";
-	//private FormBeanFactory<RequestCheckForm> formBeanFactory;
+	// private static final String ACTION_NAME = "customer-request-check.do";
+	// private static final String REQUES_CHECK_JSP =
+	// "customer-request-check.jsp";
+	// private FormBeanFactory<RequestCheckForm> formBeanFactory;
 	private CustomerDAO customerDAO;
 	private Model model;
 	public static final String NAME = "requestCheck";
+	JsonObject innerObject;
 
 	public CustomerRequestCheckAction(Model model) {
 		customerDAO = model.getCustomerDAO();
-		//formBeanFactory = FormBeanFactory.getInstance(RequestCheckForm.class);
+		// formBeanFactory =
+		// FormBeanFactory.getInstance(RequestCheckForm.class);
 		this.model = model;
 	}
 
@@ -48,18 +51,26 @@ public class CustomerRequestCheckAction extends Action {
 		List<String> errors = new ArrayList<String>();
 		request.setAttribute("errors", errors);
 		try {
-			//RequestCheckForm form;
-			//form = formBeanFactory.create(request);
-
+			// RequestCheckForm form;
+			// form = formBeanFactory.create(request);
+			innerObject = new JsonObject();
 			CustomerBean customer = (CustomerBean) request.getSession().getAttribute("customer");
 			customer = customerDAO.read(customer.getId());
 			request.setAttribute("customer", customer);
+			if (customer == null) {
+				innerObject.addProperty("message", "You must log in prior to making this request");
+				return innerObject;
+			}
 			double[] arr = model.getAmount(customer.getId());
 			double currentAmount = arr[0] / 100;
 			double validAmount = arr[2] / 100;
 			request.setAttribute("currentAmount", Util.formatNumber(currentAmount, FORMAT_STRING));
 			request.setAttribute("validAmount", Util.formatNumber(validAmount, FORMAT_STRING));
-
+			if (Double.parseDouble(amount) > validAmount) {
+				innerObject.addProperty("message",
+						"I’m sorry, the amount requested is greater than the balance of your account");
+				return innerObject;
+			}
 			/*
 			 * if (!form.isPresent()) { return REQUES_CHECK_JSP; }
 			 * 
@@ -67,20 +78,20 @@ public class CustomerRequestCheckAction extends Action {
 			 * 0) { return REQUES_CHECK_JSP; }
 			 */
 			model.commitRequestCheck(customer.getId(), ((long) (Double.parseDouble(amount) * 100)));
-			JsonObject innerObject = new JsonObject();
+
 			innerObject.addProperty("message", "The withdrawal was successfully completed");
 
 			request.setAttribute("message", "The withdrawal was successfully completed");
 			return innerObject;
-		} /*catch (FormBeanException e) {
-			e.printStackTrace();
-			return null;
-		}*/ catch (RollbackException e) {
+		} /*
+			 * catch (FormBeanException e) { e.printStackTrace(); return null; }
+			 */ catch (RollbackException e) {
 			errors.add(e.getMessage());
-			return null;
+			innerObject.addProperty("message", "I’m sorry you are not authorized to preform that action");
+			return innerObject;
 		} catch (NumberFormatException e) {
-			errors.add(e.toString());
-			return null;
+			innerObject.addProperty("message", "Number format exception found");
+			return innerObject;
 		} finally {
 			if (Transaction.isActive()) {
 				Transaction.rollback();
